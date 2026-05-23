be2769600f9c7dd3cdb139084b167433990ea166
MatteoOlarte <mateoc09+2@gmail.com>
2026-05-23 10:01:10 -0500

    week 6

--- semana 6/main.py
+++ semana 6/main.py
@@ -6,17 +6,20 @@ import tensorflow as tf
 
 # definimos las entradas y las salidas
 F = np.array([-25, 0, 50, 75, 130, 200, 220, 300])  # entradas
-C = np.array([-31.66, -17.77, 10, 23.88, 54.44, 93.33, 104.44, 148.88])  # salida
+C = np.array([-31.67, -17.78, 10, 23.89, 54.44, 93.33, 104.44, 148.89])  # salida
 
 # creamos un perceptron (solo capa entrada y capa de salida)
-layer = tf.keras.layers.Dense(units=1, input_shape=[1])  # dim salida y dim entrada
-modelo = tf.keras.Sequential([layer])
+input = tf.keras.layers.Input(shape=[1])
+HL1 = tf.keras.layers.Dense(units=3)
+HL2 = tf.keras.layers.Dense(units=3)
+output = tf.keras.layers.Dense(units=1)
+model = tf.keras.Sequential([input, HL1, HL2, output])
 
 # compilar el modelo
-modelo.compile(loss='mean_squared_error')
+model.compile(loss='mean_squared_error')
 
 # entrenamos
-historial = modelo.fit(F, C, epochs=3000, verbose=True)
+historial = model.fit(F, C, epochs=3000, verbose=True)
 
 # visualicemos la perdida
 plt.plot(historial.history['loss'])
@@ -24,7 +27,7 @@ plt.show()
 
 # testing
 valor = np.array([175])  # valor a predecir
-test = modelo.predict(valor)
+test = model.predict(valor)
 print(f"El valor de {valor} grados farenheit es igual a {test} grados celsious")
 
 
@@ -33,3 +36,8 @@ print(f"El valor de {valor} grados farenheit es igual a {test} grados celsious")
 # Un perseptron simple, con con 1K epocas el error es cercano a 0.
 # Un perseptron simple, con con 3K epocas el error es cercano a 0.
 # ----------------------------------------------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Construimos un perseptron multicapa 2 capas ocultas y 3 neuronas por capa.
+# ----------------------------------------------------------------------------------------------------------------------
+
